39b060d1bf2c366ccdcdfb886fdc94c2ade5e55e
Colm O'Shea <colmoshea@mcqs.com>
2016-08-10 14:32:35 +0100

    Solution for 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
-  #write your code here
+  def initialize
+    @seconds = 0
+  end
+  def seconds=(timeInSeconds=0)
+     @timeInSeconds = timeInSeconds
+  end
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    time_diff = Time.now() - (Time.now()- @timeInSeconds)
+    diff = Time.at(time_diff.round.abs).utc.strftime "%H:%M:%S"
+  end
 end
