e79d32dc7975e4c63cbcf732900fb2b08298a33b
Colm O'Shea <colmoshea@mcqs.com>
2016-08-10 11:30:23 +0100

    solutions for 00 - 03

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
 #write your code here
+def add (a, b)
+  a+b
+end
+
+def subtract (a, b)
+  a-b
+end
+
+def sum numbers
+  sum = 0
+  numbers.each do |i|
+    sum += i
+  end
+  sum
+end
+
+def multiply numbers
+  result = 1
+  numbers.each do |i|
+    result=result*i
+  end
+  result
+end
+
+def power (a,b)
+  a**b
+end
+
+def factorial n
+  (2..n).reduce(1, :*)
+end
